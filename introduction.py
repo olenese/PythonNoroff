7d20fc96adc178d78d0e827e133b81bea9750720
olenese <74682960+olenese@users.noreply.github.com>
2022-02-01 15:11:24 +0100

    Add files via upload

--- introduction.py
+++ introduction.py
@@ -1,7 +1,20 @@
-quotes = {
-        "Moe": "A wise guy, huh?",
-        "Larry": "Ow!",
-        "Curly": "Nyuk nyuk!",
-        }
-stooge = "Curly"
-print(stooge, "says:", quotes[stooge])
+from flask import request
+from flask import Flask
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+@app.route('/upload_file', methods =['GET', 'POST'])
+def upload_file():
+        if request.method == 'GET':
+                return 'Method not supported for this function'
+        if request.method == 'POST':
+                file = request.files['cred_dump']
+
+                if file.filename[-4:] == '.txt':
+                        file.save('credentials/{}'.format(secure_filename(file.filename)))
+                        return 'success'
+                else:
+                        return 'Error'
+
+
